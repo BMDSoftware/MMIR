24bbc41efb702d11e3a757531a8696d45e8e312c
Julian Hniopek <julian.hniopek@uni-jena.de>
2023-01-25 15:48:43 +0000

    Change Port to 5000 to conform with kubernetes and adjust compose file so it still runs under 8000 locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py makemigrations --noinput && python manage.py migrate --noinput && python manage.py runserver 0.0.0.0:8000
+python manage.py makemigrations --noinput && python manage.py migrate --noinput && python manage.py runserver 127.0.0.1:5000
