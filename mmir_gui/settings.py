3b8b9bb8b3037321fd0ee5c359b8f48ca74e007f
Julian Hniopek <julian.hniopek@uni-jena.de>
2023-02-02 16:17:01 +0000

    Fix import error

--- mmir_gui/settings.py
+++ mmir_gui/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from os import environ
-import mimetype
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
