748f314c821c563350eb1a1107a3a754b50c43fa
redg <rodrigo@ipc302.ads.uni-jena.de>
2023-02-02 18:55:39 +0100

    swap url by root in static

--- mmir_gui/settings.py
+++ mmir_gui/settings.py
@@ -139,8 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'main/media/'
 
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = 'main/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'main/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
