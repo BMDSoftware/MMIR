e0a515cfc907b2d4a2d3c45b4ca46b5eb5c1f1bc
Rodrigo Escobar Díaz Guerrero <rodrigo.escobar@uni-jena.de>
2023-02-02 09:22:04 +0000

    Update entrypoint to include collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py makemigrations --noinput && python manage.py migrate --noinput && python manage.py runserver 0.0.0.0:5000
+python manage.py makemigrations --noinput && python manage.py migrate --noinput && python manage.py collectstatic --noinput && python manage.py runserver 0.0.0.0:5000
