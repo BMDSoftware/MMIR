9939c66dd6380718292a6930b9eb3a358c2907a8
redg <rodrigo@ipc302.ads.uni-jena.de>
2023-02-16 14:11:02 +0100

    fix plugins in production adn fix proxy protocol in nginx config

--- main/apps.py
+++ main/apps.py
@@ -13,28 +13,30 @@ class MainConfig(AppConfig):
 
 
 
-    def ready(self):
-        if 'runserver' not in sys.argv:
-            return True
-        from .models import Algorithms
-        with open("main/plugins/plugins_list.json") as file:
-            data_plugins = json.load(file)
-            # load the plugins
-            print("Loading plugins ...")
-            plugin_loader.load_plugins(data_plugins["plugins"])
-            plugins_names = plugin_register.getAlgNames()
-            print("add plugins to the db ...")
-            for plugin in plugins_names:
-
-                pg_db = Algorithms.objects.filter(name=plugin)
-                if not pg_db:
-                    newAlg = Algorithms(name=plugin)
-                    newAlg.save()
-
-            print("plugins added")
-            # DELETE when is not in the plugin list
-            db_Algs = Algorithms.objects.all()
-            for db_al in db_Algs:
-                if db_al.name not in plugins_names:
-                    db_al.delete()
+    # def ready(self):
+    #     print("#"*50)
+    #     print(sys.argv)
+    #     if 'runserver' not in sys.argv:
+    #         return True
+    #     from .models import Algorithms
+    #     with open("main/plugins/plugins_list.json") as file:
+    #         data_plugins = json.load(file)
+    #         # load the plugins
+    #         print("Loading plugins ...")
+    #         plugin_loader.load_plugins(data_plugins["plugins"])
+    #         plugins_names = plugin_register.getAlgNames()
+    #         print("add plugins to the db ...")
+    #         for plugin in plugins_names:
+    #
+    #             pg_db = Algorithms.objects.filter(name=plugin)
+    #             if not pg_db:
+    #                 newAlg = Algorithms(name=plugin)
+    #                 newAlg.save()
+    #
+    #         print("plugins added")
+    #         # DELETE when is not in the plugin list
+    #         db_Algs = Algorithms.objects.all()
+    #         for db_al in db_Algs:
+    #             if db_al.name not in plugins_names:
+    #                 db_al.delete()
 
--- main/views.py
+++ main/views.py
@@ -17,11 +17,33 @@ import cv2
 import os, io, zipfile, requests
 import shutil
 import copy
+import sys
 
 pyvips.cache_set_max(0)
 
 
+if ('makemigrations' or 'migrate') not in sys.argv:
 
+    with open("main/plugins/plugins_list.json") as file:
+        data_plugins = json.load(file)
+        # load the plugins
+        print("Loading plugins ...")
+        plugin_loader.load_plugins(data_plugins["plugins"])
+        plugins_names = plugin_register.getAlgNames()
+        print("add plugins to the db ...")
+        for plugin in plugins_names:
+
+            pg_db = Algorithms.objects.filter(name=plugin)
+            if not pg_db:
+                newAlg = Algorithms(name=plugin)
+                newAlg.save()
+
+        print("plugins added")
+        # DELETE when is not in the plugin list
+        db_Algs = Algorithms.objects.all()
+        for db_al in db_Algs:
+            if db_al.name not in plugins_names:
+                db_al.delete()
 
 
 # Create your views here.
