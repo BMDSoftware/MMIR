8eab60b216863f923fd0a4ff0be21252e6ba2be2
Julian Hniopek <julian.hniopek@uni-jena.de>
2023-08-03 11:25:55 +0000

    Add health check for sql container to entrypoint. Exit with 1 if sql container is not ready, which should cause the container to be terminated and then recreated via Kubernetes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+python sql_check.py || exit 1
 nginx && python manage.py makemigrations --noinput && python manage.py migrate --noinput && python manage.py collectstatic --noinput --clear
 gunicorn mmir_gui.wsgi:application --bind 0.0.0.0:5001 --timeout 800
 
